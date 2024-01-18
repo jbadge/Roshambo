c4ed04e312e21fdb72acd69e20fb4220e5d10f3d
Jeremy Badger <jeremebadger@gmail.com>
2024-01-18 15:59:55 -0800

    Added some TS

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,27 @@
 import './style.css'
+
+let currentPlayer = 'Player One'
+let moveCounter = 0
+
+function handleClickSquare(event: MouseEvent) {
+  // event.stopPropagation()
+  // event.preventDefault()
+
+  const thingClickedOn = event.target
+
+  // NEED SOMETHING HERE TO CHANGE PLAYERS, CHECK FOR ACTIVE PLAYER
+
+  moveCounter++
+
+  const header = document.querySelector('h1')
+
+  if (header instanceof HTMLHeadingElement) {
+    header.textContent = `Move ${moveCounter}`
+  }
+}
+
+const gameBoard = document.querySelector('ul')
+
+if (gameBoard instanceof HTMLUListElement) {
+  gameBoard.addEventListener('click', handleClickSquare)
+}
